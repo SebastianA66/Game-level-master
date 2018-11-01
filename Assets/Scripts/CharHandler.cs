8ba4d3d417b65fc42761576ce8c957b4fc139e4e
SebastianA66 <42424010+SebastianA66@users.noreply.github.com>
2018-11-01 11:18:48 +1100

    Initial commit

---
+++ Assets/Scripts/CharHandler.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharHandler : MonoBehaviour
+{
+    public bool alive;
+    public float maxHealth;
+    public float curHealth;
+    public GUIStyle healthBar;
+
+    private void Start()
+    {
+        curHealth = maxHealth;
+        alive = true;
+        
+    }
+
+    private void LateUpdate()
+    {
+        if (curHealth > maxHealth)
+        {
+            curHealth = maxHealth;
+        }
+        if(curHealth < 0 || !alive)
+        {
+            curHealth = 0;
+        }
+        if (alive && curHealth == 0)
+        {
+            alive = false;
+            
+        }
+    }
+}
